ae3d3410a5085ac673ea70d112c1a3c9213a1e58
Hubert Wiśniewski <hubert_w@autograf.pl>
2018-10-03 18:25:18 +0200

    Navbar

--- main.js
+++ main.js
@@ -1,22 +1,2 @@
 
-var last_known_scroll_position = 0;
-var aboutMe = document.querySelector('.aboutMe')
 
-window.addEventListener('scroll', function(e) {
-
-    last_known_scroll_position = window.scrollY;
-  
-    if (last_known_scroll_position > 106) {
-  
-      window.requestAnimationFrame(function() {
-        var aboutMe = document.querySelector('.aboutMe')
-        aboutMe.style.opacity = 1;
-        console.log('KJeaa')
-        // ticking = false;
-      });
-       
-    //   ticking = true;
-  
-    }
-    
-  });
