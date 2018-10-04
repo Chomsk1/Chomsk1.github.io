3337a131e287e374c91a7c42d94e1b2143951b71
Hubert Wiśniewski <hubert_w@autograf.pl>
2018-10-04 23:59:54 +0200

    jll

--- game.js
+++ game.js
@@ -1,6 +1,41 @@
-var player = document.querySelector('.player')
+// var player = document.querySelector('.player')
+// var direction = 0;
+// update ();
+
+// window.addEventListener('keydown', function(event){
+// var key = event.code
+
+// if (key === 'ArrowRight') {
+//  direction = 1;
+// }
+
+// if (key === 'ArrowLeft') {
+//     direction = -1;
+// }
 
 
-window.addEventListener(keydown, function(){
-    
-})
+// })
+
+// window.addEventListener('keyup', function (event){
+// var key = event.code
+
+// if (key === 'ArrowRight') {
+//     direction = 0;
+// }
+// if (key === 'ArrowLeft') {
+//     direction = 0;
+// }
+
+// })
+
+
+// function update() {
+
+//     if (direction = 1) {
+//         player.style.marginLeft = direction * 2 + 'px'
+//     }
+
+
+
+//     requestAnimationFrame(update);
+// }
